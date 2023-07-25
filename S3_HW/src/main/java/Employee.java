2afba6adb8009124b99e82e1c21ea676cf314d58
Nick <mojiotok167@gmail.com>
2023-07-25 20:38:43 +0300

    add 1.1-4 tasks in third homework

--- S3_HW/src/main/java/Employee.java
+++ S3_HW/src/main/java/Employee.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class Employee {
+import lombok.Setter;
+
+@Setter
+//@AllArgsConstructor
+public class Employee {
+    /*
+    * – Создать класс ”Сотрудник”с полями: ФИО, должность, телефон, зарплата, возраст;
+    – Написать метод выводящую всю доступную информацию об объекте с использованием форматирования строк.
+    – Создать массив из 5 сотрудников
+    – Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000. Возраст и размер
+    * повышения должны быть параметрами метода.
+    – Написать тот же метод повышения в качестве статического в классе сотрудника. Метод должен
+    * принимать в качестве параметра массив сотрудников.
+    – Написать методы (принимающие на вход массив сотрудников), вычисляющие средний возраст и
+    * среднюю зарплату сотрудников, вывести результаты работы в консоль.
+    2. Опишите класс руководителя, наследник от сотрудника. Перенесите статический метод повышения
+    * зарплаты в класс руководителя, модифицируйте метод таким образом, чтобы он мог поднять заработную
+    * плату всем, кроме руководителей. В основной программе создайте руководителя и поместите его в
+    * общий массив сотрудников.
+    * Повысьте зарплату всем сотрудникам и проследите, чтобы зарплата руководителя не повысилась.
+    * */
+    String name;
+    String midName;
+    String surName;
+    String position;
+    String phone;
+    int salary;
+    int age;
+
+    private static final int CURREANT_YEAR = 2023;
+
+    public Employee(String name, String midName, String surName, String position, String phone, int salary, int age) {
+        this.name = name;
+        this.midName = midName;
+        this.surName = surName;
+        this.position = position;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return CURREANT_YEAR - age;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee {name= %s, midname= %s, surname= %s, position= %s, phone= %s, salary= %d, age= %d", name, midName, surName, position, phone, salary, getAge());
+        //return String.format("Employee {name= %s, midname= %s, surname= %s, position= %s, phone= %s,", name, midName, surName, position);
+    }
 }
--- S3_HW/src/main/java/Main.java
+++ S3_HW/src/main/java/Main.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        Employee ivan = new Employee("Ivan", "Aleksandrovich", "Bubkov", "CEO", "1", 1000000, 1980);
+        employees.add(ivan);
+        employees.add(new Employee("Nickolas", "Aleksandrovich", "Sabakevich", "BOSS", "001", 9999999, 1996));
+        employees.add(new Employee("Olga", "Victorovna", "Vasilieva", "accountant", "2", 200000, 1990));
+        employees.add(new Employee("Vasilisa", "Nikolaevna", "Borisova", "assistant", "3", 100000, 2000));
+        employees.add(new Employee("Egor", "Olegovich", "Shmatko", "Head of Security", "4", 150000, 1980));
+
+
+        getAllInfo(employees);
+
+        increaseSalary(employees, 30, 5000);
+
+        getAllInfo(employees);
+
+
+    }
+
+    private static void increaseSalary(ArrayList<Employee> employees, int age, int increase) {
+        for (Employee employee : employees) {
+            if (employee.getAge() > age) {
+                employee.setSalary(employee.getSalary() + increase);
+            }
+
+        }
+    }
+
+    private static void getAllInfo(ArrayList<Employee> employees) {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+
+
+
+
+
 }
